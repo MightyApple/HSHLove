54c2c4ba0288043fd0a87098bc49db9ec494166a
MightyApple <bugrell.aylin@web.de>
2023-04-15 20:53:16 +0200

    express server da

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>TEST</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
