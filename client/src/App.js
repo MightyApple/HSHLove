ca7bb84ca877eab72074b50f1eea35382793ac15
MightyApple <bugrell.aylin@web.de>
2023-05-22 22:16:20 +0200

    Nachrichten werden in DB gespeichert & Chats werden angezeigt

--- client/src/App.js
+++ client/src/App.js
@@ -20,11 +20,32 @@ import ErrorPage from "./error-page";
 import { socket } from './components/socket';
 import { useState } from 'react';
 
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for(let i = 0; i <ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+
 function App() {
-    const [loggedIn, setLoggedIn] = useState(false); //TODO: muss mit session/cookie synchronisiert werden
+    const [loggedIn, setLoggedIn] = useState(sessionCheck()); //TODO: muss mit session/cookie synchronisiert werden
 
     const [chatMessages, setChatMessages] = useState([]);
     const [matchedUsers, setMatchedUsers] = useState([]);
+
+    function sessionCheck() {
+        var cookieValue = getCookie("loggedIn");
+        return cookieValue;
+    }
 
     useEffect(() => { // wird einmal ausgeführt
         if (loggedIn) { // wenn der user eingeloggt ist
