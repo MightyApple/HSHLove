6dff82a12baf43c87c534318c3509b3741f49a5c
PelzigerPudding <keith.strasdas@stud.hshl.de>
2023-06-06 22:48:00 +0200

    chat style desktop

--- client/src/pages/ChatUserList.jsx
+++ client/src/pages/ChatUserList.jsx
@@ -2,9 +2,14 @@ import React, { useMemo, useState, useEffect } from 'react'
 import Chat from './Chat';
 import Navbar from '../components/navbar';
 import "./ChatUserList.css";
+import "./startingPage.css"
 
 import Trenner from '../components/trenner'
 import UserBanner from  '../components/userBanner'
+import {useMediaQuery} from "react-responsive";
+
+import logo from "../assets/logo.svg";
+
 
 export default function ChatUserList({ chatRooms, matchedUsers }) {
     const [selectedUser, setSelectedUser] = useState(null);
@@ -13,8 +18,57 @@ export default function ChatUserList({ chatRooms, matchedUsers }) {
 
 
 
+    const isWideScreen = useMediaQuery({minWidth: 769});
 
-    if (selectedUser) { // wenn ein user ausgewählt wurde, dann wird der Chat gerendert
+    if (isWideScreen && selectedUser) {
+        return (
+            <>
+                <Navbar></Navbar>
+                <div className={"wideChat"}>
+                    <div>
+                        {matchedUsers.map((user, index) => (
+                            <div key={index} onClick={() => setSelectedUser(user)}>
+                                <UserBanner user={user}></UserBanner>
+                            </div>
+                        ))}
+                        <div className={"primaryContainer"}>
+                            <Trenner></Trenner>
+                            <p>Du möchtest dich mit mehr Menschen in Kontakt setzen? <br/><br/>
+                                Dann schau dir weitere Profile an oder bearbeite dein Profil.</p>
+                        </div>
+                    </div>
+                    <div className={"wideChatSide"}>
+                        <UserBanner user={selectedUser}></UserBanner>
+                        <Chat chatMessages={selectedUserMessages} receiver={selectedUser}></Chat>
+                    </div>
+                </div>
+            </>
+        )
+    } else if (isWideScreen) {
+        return (
+            <>
+                <Navbar></Navbar>
+                <div className={"wideChat"}>
+                    <div>
+                        {matchedUsers.map((user, index) => (
+                            <div key={index} onClick={() => setSelectedUser(user)}>
+                                <UserBanner user={user}></UserBanner>
+                            </div>
+                        ))}
+                        <div className={"primaryContainer"}>
+                            <Trenner></Trenner>
+                            <p>Du möchtest dich mit mehr Menschen in Kontakt setzen? <br/><br/>
+                                Dann schau dir weitere Profile an oder bearbeite dein Profil.</p>
+                        </div>
+                    </div>
+                    <div className={'wideChatSide startinLogo bigHeadline'}>
+                        <img src={logo} alt={'logo'} width={168} height={168}/>
+                        <p>HSH<span>Love</span></p>
+                    </div>
+                </div>
+            </>
+        )
+    } else if (selectedUser) { // wenn ein user ausgewählt wurde, dann wird der Chat gerendert
         return (
             <>
                 <Navbar></Navbar>
