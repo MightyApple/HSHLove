4349bd89c9fc5fb47fe805c44d5b09a0d4504d5a
Christopher Seibel <christopher.seibel@iem.fraunhofer.de>
2023-06-06 03:07:48 +0200

    login enter button

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -13,6 +13,19 @@ export default function Root({ setLoggedIn }) {
         navigate('/')
     }
     
+    // Get the input field
+    var input = document.getElementById("password");
+
+    // Execute a function when the user presses a key on the keyboard
+    input.addEventListener("keypress", function(event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("submitBtn").click();
+    }
+    });
 
     const submitForm = async ()=>{
         let email = document.getElementById("email");
